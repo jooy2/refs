4d452be7320acc801973b3bcb9d493a14981c0c4
leejooy96 <leejooy96@gmail.com>
2023-07-01 14:52:12 +0900

    package: use clean urls in vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,6 +12,7 @@ export default {
 		['link', { rel: 'icon', href: '/logo.png' }],
 		['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
 	],
+	cleanUrls: true,
 	themeConfig: {
 		sidebar: generateSidebar({
 			root: 'docs',
