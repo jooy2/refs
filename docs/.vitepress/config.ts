593c950974eb841f1a21d57fc5592ad26272c774
leejooy96 <leejooy96@gmail.com>
2024-09-10 14:10:03 +0900

    package: change vitepress configurations

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,19 +1,33 @@
 import { generateSidebar } from 'vitepress-sidebar';
 import { defineConfig } from 'vitepress';
-import { name, description, repository } from '../../package.json';
+import { generateI18nLocale, generateI18nSearch } from 'vitepress-i18n';
+import { name, description, repository, homepage } from '../../package.json';
 
 const capitalizeFirst = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const defaultLocale: string = 'ko';
+const defineSupportLocales = [{ label: defaultLocale, translateLocale: defaultLocale }];
+
+const editLinkPattern = 'https://github.com/jooy2/refs/edit/master/docs/:path';
 
 // Ref: https://vitepress.vuejs.org/config/introduction
 export default defineConfig({
 	title: capitalizeFirst(name),
+	lastUpdated: true,
 	description,
 	outDir: '../dist',
+	cleanUrls: true,
+	metaChunk: true,
+	rewrites: {
+		'ko/:rest*': ':rest*'
+	},
 	head: [
 		['link', { rel: 'icon', href: '/logo.png' }],
 		['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
 	],
-	cleanUrls: true,
+	sitemap: {
+		hostname: homepage
+	},
 	themeConfig: {
 		sidebar: generateSidebar({
 			documentRootPath: 'docs',
@@ -21,15 +35,32 @@ export default defineConfig({
 			useTitleFromFileHeading: true,
 			capitalizeFirst: true
 		}),
-		search: {
+		search: generateI18nSearch({
+			defineLocales: defineSupportLocales,
+			rootLocale: defaultLocale,
 			provider: 'local'
-		},
+		}),
 		nav: [
 			{
 				text: 'Blog',
 				link: 'https://jootc.com'
 			}
 		],
-		socialLinks: [{ icon: 'github', link: repository.url.replace('.git', '') }]
+		socialLinks: [{ icon: 'github', link: repository.url.replace('.git', '') }],
+		footer: {
+			message: 'Released under the MIT License',
+			copyright: '© <a href="https://jooy2.com">Jooy2</a>'
+		},
+		locales: generateI18nLocale({
+			label: {
+				ko: '한국어'
+			},
+			lang: {
+				ko: 'ko-KR'
+			},
+			defineLocales: defineSupportLocales,
+			rootLocale: defaultLocale,
+			editLinkPattern
+		})
 	}
 });
